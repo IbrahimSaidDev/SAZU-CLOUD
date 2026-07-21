6ef0580e06e60c708892127dcb02c53465b512ef
IbrahimSaidDev <ibrahimsaid4031970@gmail.com>
2026-07-22 01:39:36 +0200

    Add user and message routes to server

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import userRoutes from "./routes/users.js";
+import messageRoutes from "./routes/messages.js";
+
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/users", userRoutes);
+app.use("/messages", messageRoutes);
 
 app.get("/", (req, res) => {
   res.send("SAZU Cloud Backend is Running");
@@ -16,5 +22,5 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`SAZU Cloud Server running on port ${PORT}`);
 });
