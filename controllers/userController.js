d08be0d7a83b36e59e61c756796ec0ae0360883c
IbrahimSaidDev <ibrahimsaid4031970@gmail.com>
2026-07-22 13:10:31 +0200

    Implement getUsers function in userController

---
+++ controllers/userController.js
@@ -0,0 +1,19 @@
+import { db } from "../config.js";
+‎
+‎export const getUsers = async (req, res) => {
+‎  try {
+‎    const users = await db.collection("users").get();
+‎
+‎    const data = users.docs.map((doc) => ({
+‎      id: doc.id,
+‎      ...doc.data()
+‎    }));
+‎
+‎    res.json(data);
+‎
+‎  } catch (error) {
+‎    res.status(500).json({
+‎      error: error.message
+‎    });
+‎  }
+‎};
